ce63caffb5e99af522e74737eb2f5b38046d1c30
Michal Brzoznowki <michal.brzoznowski@gmail.com>
2017-05-23 23:41:06 +0200

    Two methods - same result

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,1 +1,13 @@
 
+def sum_odd(n)
+  (1..n).inject do |sum, i |
+    i.odd? ? sum + i : sum
+  end
+end
+
+def sum_odd_2(n)
+  (1..n).inject{|sum, i| i.odd? ? sum + i : sum}
+end
+
+puts sum_odd(51)
+puts sum_odd_2(51)
